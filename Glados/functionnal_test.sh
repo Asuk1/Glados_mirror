cdf0d2a565629c0243ea9a45f58e35ca74f0a1e5
Brocart Guillaume <guillaume.brocart@epitech.eu>
2023-12-20 01:00:51 +0100

    feat(Test): add functionnal_test.sh

---
+++ Glados/functionnal_test.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+check_executable() {
+    local current_directory="$(pwd)"
+    local executable_name="$current_directory/glados"
+    local executable="$1"
+
+    if command -v "$executable" >/dev/null 2>&1; then
+        echo -e "\033[32m✔\033[0m: L'exécutable glados est présent."
+        return 0
+    else
+        echo -e "\033[31m✘\033[0m: Erreur : L'exécutable glados n'est pas trouvé."
+        make re
+        if command -v "glados" >/dev/null 2>&1; then
+            echo -e "\033[32m✔\033[0m: L'exécutable glados a été créé avec succès."
+            return 0
+        else
+            echo -e "\033[31m✘\033[0m: Erreur : Impossible de créer l'exécutable glados avec make."
+            exit 1
+        fi
+    fi
+}
+
+
+run_test() {
+    local file_path="$1"
+    local expected_output="$2"
+    local title="$3"
+    local executable="./glados"
+
+    actual_output="$($executable "$file_path")"
+
+
+    if [ "$actual_output" == "$expected_output" ]; then
+        echo -e "\033[32m✔\033[0m: $title"
+        return 0
+    else
+        echo -e "\033[31m✘\033[0m: $title"
+        return 1
+    fi
+}
+
+
+
+
+check_executable "$executable_name"
+
+
+run_test "Path/File.scm" "4" "2 * 2"
