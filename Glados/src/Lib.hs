26d10e1e1e54377562e1e696f650698b24f9875d
Thomas FIANCETTE <thomas.fiancette@epitech.eu>
2023-12-20 09:46:32 +0100

    [ADD] project base and adding subtract

---
+++ Glados/src/Lib.hs
@@ -0,0 +1,21 @@
+module Lib
+    ( someFunc,
+        Ast (..),
+        Env,
+    ) where
+
+someFunc :: IO ()
+someFunc = putStrLn "someFunc"
+
+
+
+data Ast = AstInteger Int
+    | AstSymbol String
+    | AstBoolean String
+    | AstCall [Ast]
+    | AstDefine (Either String [String]) Ast
+    | AstLambda [String] Ast deriving (Show)
+
+
+
+type Env = [(String, Ast)]
