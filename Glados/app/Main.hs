31ae50d2cce3818fadd51fbc401e50ad8c162e33
Evan Labourdette <evan.labourdette@epitech.eu>
2024-01-02 14:15:07 +0100

    feat(Error handling): fix error handling

--- Glados/app/Main.hs
+++ Glados/app/Main.hs
@@ -9,16 +9,10 @@ module Main (main) where
 
 import ReadAndProcess
 import System.Environment (getArgs)
-import ErrorHandling (error_handling)
-import System.Exit (exitWith, ExitCode(..))
 
 main :: IO ()
 main = do
     args <- getArgs
     case args of
-        [filename] -> do
-            error_handling filename
-            readFileAndProcess filename
-        _ -> do
-            putStrLn "Wrong number of arguments. Usage: glados <filename>"
-            exitWith (ExitFailure 84)
+        [filename] -> readFileAndProcess filename
+        _ -> readStdinAndProcess
