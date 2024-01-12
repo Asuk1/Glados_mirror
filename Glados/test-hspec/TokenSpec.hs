b1e7cc054da6aeb07686564749f55e1f81f72689
Baptiste PERARD <baptiste.perard@epitech.eu>
2024-01-12 20:15:22 +0100

    feat(Unit test syntax error): Adding unit test to test the function checkParenthesis

--- Glados/test-hspec/TokenSpec.hs
+++ Glados/test-hspec/TokenSpec.hs
@@ -18,6 +18,22 @@ testTokenizeString = do
             let string = "define add (lambda (a b) (+ a b));\nadd 3 4;"
             stringToToken string `shouldBe` (["define", "add", "(", "lambda", "(", "a", "b", ")", "(", "+", "a", "b", ")", ")", ")", "(", "add", "3", "4", ")"])
 
+testCheckParenthesis :: Spec
+testCheckParenthesis = do
+    describe "Check Parentheses" $ do
+        it "Empty input" $ do
+            checkParenthesis [] `shouldBe` (0, 0)
+        it "Single opening parenthesis" $ do
+            checkParenthesis ["("] `shouldBe` (1, 0)
+        it "Single closing parenthesis" $ do
+            checkParenthesis [")"] `shouldBe` (0, 1)
+        it "Balanced parentheses" $ do
+            checkParenthesis ["(", ")", "(", ")"] `shouldBe` (2, 2)
+        it "Unbalanced parentheses" $ do
+            checkParenthesis ["(", "(", ")"] `shouldBe` (2, 1)
+
+
 spec :: Spec
 spec = do
     testTokenizeString
+    testCheckParenthesis
