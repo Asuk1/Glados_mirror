e8a371f87425c1f8a27a3baa67272c20582d4a8a
Baptiste PERARD <baptiste.perard@epitech.eu>
2024-01-12 19:22:44 +0100

    fix(Functional and unit test): Fix value of file and value of functional test, fix test for Tokenization part

--- Glados/test-hspec/TokenSpec.hs
+++ Glados/test-hspec/TokenSpec.hs
@@ -9,13 +9,14 @@ testTokenizeString :: Spec
 testTokenizeString = do
     describe "Tokenize with valid string" $ do
         it "Tokenize simple line" $ do
-            let string = "(define x 3)"
-            stringToToken string `shouldBe` (["(", "define", "x", "3", ")"])
+            let string = "define x 3;"
+            stringToToken string `shouldBe` (["define", "x", "3", ")"])
         it "Tokenize simple line with \\n" $ do
-            let string = "(define foo 21)\n(* foo 2)"
-            stringToToken string `shouldBe` (["(", "define", "foo", "21", ")", "(", "*", "foo", "2", ")"])
-
-
+            let string = "define foo 21;\n* foo 2;"
+            stringToToken string `shouldBe` (["define", "foo", "21", ")", "(", "*", "foo", "2", ")"])
+        it "Tokenize complex line with \\n" $ do
+            let string = "define add (lambda (a b) (+ a b));\nadd 3 4;"
+            stringToToken string `shouldBe` (["define", "add", "(", "lambda", "(", "a", "b", ")", "(", "+", "a", "b", ")", ")", ")", "(", "add", "3", "4", ")"])
 
 spec :: Spec
 spec = do
