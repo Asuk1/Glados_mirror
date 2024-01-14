4aaf96a477ae5491b776f23ee6c4e1fa9551564f
Baptiste PERARD <baptiste.perard@epitech.eu>
2024-01-14 19:18:31 +0100

    feat(succ and bigger): Adding env to take function (succ), adding instruction to manage function define in 'define' structure of the new langages, adding builtin bigger

--- Glados/src/Vm.hs
+++ Glados/src/Vm.hs
@@ -26,7 +26,7 @@ module Vm (
 import Data.Maybe (mapMaybe)
 
 data Value = VInt Int | VBool Bool | VOp Op | VFunc [Instruction] deriving (Show, Eq)
-data Op = Add | Sub | Mul | Div | Less | Fact | Succ | Equal deriving (Show, Eq)
+data Op = Add | Sub | Mul | Div | Less | Bigger | Fact | Succ | Equal deriving (Show, Eq)
 data Instruction = Push Value | Pop | Call | Ret | JumpIfFalse Int | PushArg Int | PushEnv String | Define deriving (Show, Eq)
 type Stack = [Value]
 type Env = [(String, Value)]
@@ -46,6 +46,7 @@ opToFunction Sub = Right subOp
 opToFunction Mul = Right (*)
 opToFunction Div = Right divOp
 opToFunction Less = Right (\a b -> if a < b then 1 else 0)
+opToFunction Bigger = Right (\a b -> if a > b then 1 else 0)
 opToFunction Equal = Right (\a b -> if a == b then 1 else 0)
 opToFunction Fact = Left factOp
 opToFunction Succ = Left (\a -> a + 1)
@@ -67,12 +68,12 @@ divOp b a
 subOp :: BinaryOp
 subOp b a = a - b
 
-executeInstruction :: Instruction -> Stack -> [Value] -> Env -> Either String (Stack, Int)
-executeInstruction Define stack _ _ = Right (stack, 1)
-executeInstruction (Push value) stack _ _ = Right (value : stack, 1)
-executeInstruction Pop [] _ _ = Left "Error: Not enough arguments on stack"
-executeInstruction Pop (_:stack) _ _ = Right (stack, 1)
-executeInstruction Call (VOp op : stack) _ _ =
+executeInstruction :: Instruction -> [Instruction]-> Stack -> [Value] -> Env -> Either String (Stack, Int)
+executeInstruction Define _ stack _ _ = Right (stack, 1)
+executeInstruction (Push value) _ stack _ _ = Right (value : stack, 1)
+executeInstruction Pop _ [] _ _ = Left "Error: Not enough arguments on stack"
+executeInstruction Pop _ (_:stack) _ _ = Right (stack, 1)
+executeInstruction Call _ (VOp op : stack) _ _ =
     case opToFunction op of
         Left unaryOp ->
             case stack of
@@ -82,19 +83,23 @@ executeInstruction Call (VOp op : stack) _ _ =
             case stack of
                 (VInt a : VInt b : rest) -> Right (VInt (binaryOp a b) : rest, 1)
                 _ -> Left "Error: Invalid arguments for binary operation"
-executeInstruction Call (VFunc func : stack) args env =
+executeInstruction Call _ (VFunc func : stack) args env =
     case exec func [] args env 0 of
         Right retStack -> Right (head retStack : stack, 1)
         Left errorMsg -> Left errorMsg
-executeInstruction Ret stack _ _ = Right (stack, 1)
-executeInstruction (JumpIfFalse offset) (VBool False : stack) _ _ = Right (stack, offset)
-executeInstruction (JumpIfFalse _) stack _ _ = Right (stack, 1)
-executeInstruction (PushArg index) stack args _ =
+executeInstruction Ret _ stack _ _ = Right (stack, 1)
+executeInstruction (JumpIfFalse offset) _ (VBool False : stack) _ _ = Right (stack, offset)
+executeInstruction (JumpIfFalse _) _ stack _ _ = Right (stack, 1)
+executeInstruction (PushArg index) _ stack args _ =
     case drop index args of
         (arg:_) -> Right (arg : stack, 1)
         [] -> Left "Error: Argument index out of bounds"
-executeInstruction (PushEnv name) stack _ env =
+executeInstruction (PushEnv name) instructions stack _ env =
     case lookup name env of
+        Just (VFunc funcInstructions) ->
+            case exec funcInstructions stack [] (createEnv instructions stack) 0 of
+                Right retStack -> Right (retStack, 1)
+                Left errorMsg -> Left errorMsg
         Just value -> Right (value : stack, 1)
         Nothing -> Left "Error: Variable not found"
 
@@ -107,8 +112,8 @@ exec :: [Instruction] -> Stack -> [Value] -> Env -> Int -> Either String Stack
 exec instructions stack args env ip
     | ip >= length instructions = Right stack
     | otherwise =
-        case executeInstruction (instructions !! ip) stack args env of
-            Right (newStack, offset) -> exec instructions newStack args env (ip + offset)
+        case executeInstruction (instructions !! ip) instructions stack args env of
+            Right (newStack, offset) -> exec instructions newStack args (createEnv instructions newStack) (ip + offset)
             Left errorMsg -> Left errorMsg
 
 parseInstruction :: String -> Maybe Instruction
@@ -132,17 +137,22 @@ parseOp op = case op of
     "Mul" -> Mul
     "Div" -> Div
     "Less" -> Less
-    "Fact" -> Fact
     "Equal" -> Equal
-    "Succ" -> Succ
+    "Bigger" -> Bigger
     _ -> error "Unknown operation"
 
-createEnv :: [Instruction] -> Env
-createEnv instructions = go instructions []
+createEnv :: [Instruction] -> Stack -> Env
+createEnv instructions stack = go instructions stack []
   where
-    go [] env = env
-    go (Define : PushEnv var : Push value : rest) env = go rest ((var, value) : env)
-    go (_ : rest) env = go rest env
+    go :: [Instruction] -> Stack -> Env -> Env
+    go [] _ env = env
+    go (Define : PushEnv var : Push value : rest) stack env =
+        go rest stack ((var, value) : env)
+    go (PushEnv name : PushEnv var : x : y : z : j : rest) stack env =
+        go rest stack ((name, VFunc [x, y, z, j]) : (var, (head (if null stack then [VInt 0] else stack))) : env)
+    go (Push (VOp Fact) : PushEnv var : rest) stack env =
+        go rest stack ((var, VFunc [Push (VOp Fact)]) : env)
+    go (_ : rest) stack env = go rest stack env
 
 astToInstructions :: Ast -> [Instruction]
 astToInstructions (AstInteger n) = [Push (VInt n)]
@@ -151,15 +161,18 @@ astToInstructions (AstSymbol "-") = [Push (VOp Sub)]
 astToInstructions (AstSymbol "*") = [Push (VOp Mul)]
 astToInstructions (AstSymbol "/") = [Push (VOp Div)]
 astToInstructions (AstSymbol "<") = [Push (VOp Less)]
+astToInstructions (AstSymbol ">") = [Push (VOp Bigger)]
 astToInstructions (AstSymbol "=") = [Push (VOp Equal)]
-astToInstructions (AstSymbol "fact") = [Push (VOp Fact)]
-astToInstructions (AstSymbol "succ") = [Push (VOp Succ)]
 astToInstructions (AstSymbol s) = [PushEnv s]
 astToInstructions (AstBoolean b) = [Push (VBool (read b))]
 astToInstructions (AstDefine (Left var) body) =
     [Define] ++ PushEnv var : astToInstructions body
+astToInstructions (AstCall [AstSymbol "define", AstCall [AstSymbol func, AstSymbol var], rest]) =
+    [Define] ++ [PushEnv func] ++ PushEnv var : astToInstructions rest
 astToInstructions (AstCall [name, arg1, arg2]) =
     astToInstructions arg1 ++ astToInstructions arg2 ++ astToInstructions name ++ [Call]
+astToInstructions (AstCall [AstSymbol name, arg1]) =
+    astToInstructions arg1 ++ [PushEnv name]
 astToInstructions (AstCall astList) = concatMap astToInstructions astList
 astToInstructions (AstDefine (Right argList) body) =
     astToInstructions (AstCall [AstDefine (Right argList) body])
@@ -187,8 +200,8 @@ executer filePath = do
     let tmp = removeUnwantedChars contents
     let instruction = mapMaybe parseInstruction (lines tmp)
 
-    let env = createEnv instruction
     let initialStack = []
+    let env = createEnv instruction initialStack
 
     case exec instruction initialStack [VInt (-42)] env 0 of
         Right resultStack -> case resultStack of
